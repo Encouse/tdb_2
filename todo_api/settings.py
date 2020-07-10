bfd3a46f4d657dcaa9eac4f53df5b67a99ffd381
Encouse <54453362+Encouse@users.noreply.github.com>
2020-07-10 16:29:07 +0300

    Update settings.py

--- todo_api/settings.py
+++ todo_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -148,3 +149,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
