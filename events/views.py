c712717a2d6b2b83ddcccb48f9ded9ea8ab7ef32
Encouse <54453362+Encouse@users.noreply.github.com>
2020-07-10 18:39:06 +0300

    Update views.py

--- events/views.py
+++ events/views.py
@@ -5,6 +5,7 @@ from rest_framework import viewsets
 from rest_framework import filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.response import Response
 from rest_framework import status
 from main_app.views import MethodModelViewSet
 from main_app import models
